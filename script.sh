4f4ca430c6be30d1111769abd59246beb39c31aa
princejoogie <princejoogie@gmail.com>
2022-03-30 04:27:00 +0800

    update to react 18

--- script.sh
+++ script.sh
@@ -3,12 +3,11 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
-    # Proceed with the build
-    echo "✅ - Build can proceed"
-    exit 1
-
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1
 else
-    # Don't build
-    echo "🛑 - Build cancelled"
-    exit 0
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0
 fi
